bb4018d3376fc624b2743e5509a779be483160d5
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-04-20 12:25:57 +0200

    deploy on github pages

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let tg = window.Telegram.WebApp
 
 let init = document.getElementById('init')
 let div = document.createElement('div');
-div.innerHTML = tg.initDataUnsafe;
+div.innerHTML = `${tg.initDataUnsafe}`;
 init.appendChild(div);
